ecf87f6ee2cec0f0e15b176002e5ddee5b92391b
vurrut <calvoredondo@gmail.com>
2012-06-01 11:07:21 +0200

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 PREEMPT Thu Mar 15 23:24:32 PDT 2012"
+#define LINUX_COMPILE_BY "mgq"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
